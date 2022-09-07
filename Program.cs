e474982977727e4b28dba48d398738d408af2355
Hilbnwal <103248931+Hilbnwal@users.noreply.github.com>
2022-09-07 19:57:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace work2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			try
+			{
+				if (args.Length == 0)
+					throw new Exception("Отсутствует входной аргумент!");
+				int timeout = int.Parse(args[0]);
+				CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+				CancellationToken token = cancelTokenSource.Token;
+				Task task = Task.Run(() => Processing(token));
+				Thread.Sleep(timeout);
+				cancelTokenSource.Cancel();
+				task.Wait();
+			}
+
+
+			catch (FormatException)
+			{
+				Console.WriteLine("Входной аргумент неверен!");
+			}
+
+
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+		static void Processing(CancellationToken token)
+		{
+			Thread.Sleep(100);
+			if (token.IsCancellationRequested)
+			{
+				Console.WriteLine("Задача выполнена");
+				return;
+			}
+			Console.WriteLine("Задача выполняется");
+			Thread.Sleep(100);
+			Console.WriteLine("Задача выполнена");
+
+
+		}
+    }
+}
